3604f71d3337824579e8b3b214a7baf2d5320852
Vitaly-IS-23 <vitalie01rus01@gmail.com>
2021-04-18 13:07:03 +0300

    Добавьте файлы проекта.

---
+++ CarLibrary/DerivedCars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;  
+namespace CarLibrary
+{
+    public class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int maxSp, int currSp)
+        : base(name, maxSp, currSp) { }
+        public override void TurboBoost()  
+        {
+            MessageBox.Show("Таранная скорость!", "Чем быстрее, тем лучше...");
+        }
+    }
+    public class MiniVan : Car
+    {
+        public MiniVan() { }  
+        public MiniVan(string name, int maxSp, int currSp)
+        : base(name, maxSp, currSp) { } 
+        public override void TurboBoost() 
+        {
+            egnState = EngineState.engineDead;
+            MessageBox.Show("Упс!", "Ваш блок двигателя взорвался!");
+        }
+    }
+}
